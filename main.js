e00c52075024d45eb47402a07dd38095edf2ba2d
Martina Deutschová <deutschova.martina@gmail.com>
2021-04-06 20:03:46 +0200

    vyplněno

--- main.js
+++ main.js
@@ -6,24 +6,20 @@
 // Úkol 1 - Založ nové pole se seznamem jmen (alespoň 5)
 // Vypiš jeho obsah do stránky - JavaScript spojí pole automaticky do řetězce
 
-
-
-
+let seznamJmen = ['Bart', 'Polly', 'Mína', 'Peggy', 'Líza', 'Mikeš', 'Hopsinka']
+let vystup = document.querySelector('#vystup')
+console.log(seznamJmen)
+vypisText(seznamJmen)
 
 // Úkol 2 - Vypiš třetí jméno v pořadí
 // Nezapomeň, že pole se čísluje od 0
-
-
-
-
-
+console.log(seznamJmen[2])
+vypisText(seznamJmen[2])
 
 // Úkol 3 - Kolik jmen je v poli?
 // Pole má vlastnost pole.length
-
-
-
-
+console.log(seznamJmen.length)
+vypisText(seznamJmen.length)
 
 // Úkol 4 - Vypiš znovu seznam jmen, ALE...
 // Tentokrát použij cyklus .forEach() a před každé jméno přidej
@@ -41,19 +37,20 @@
 // pole.push()
 // pole.unshift()
 // Doplněný seznam jmen vypiš (jednoduše jako v příkladu 1)
-
-
-
+seznamJmen.push('Bella')
+vypisText(seznamJmen)
+console.log(seznamJmen)
 
 
 // Úkol 6 - odeber z pole první a poslední jméno a vypiš
 // obě jména a pak i výsledné pole
 // pole.shift()
 // pole.pop()
-
-
-
-
+vypisText(seznamJmen[0])
+vypisText(seznamJmen[6])
+seznamJmen.shift()
+seznamJmen.pop()
+vypisText(seznamJmen)
 
 
 
@@ -63,4 +60,5 @@
 // *********************************************
 function vypisText(txt) {
     document.querySelector('#vystup').innerText += txt + '\n';
+    
 }
